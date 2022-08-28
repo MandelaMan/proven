0eacb18324a35280368079226e0fcb4f5ba88570
MandelaMan <nelsonomoro@gmail.com>
2022-08-28 11:15:54 +0300

    fixed mail

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -5,15 +5,39 @@ const mail = require("@sendgrid/mail");
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default (req, res) => {
-  const message = `New service request from proven.africa`;
+  const body = req.body;
+
+  const message = `Dear Proven,\r\n\n
+                   You have recieved a new client request\r\n
+                   Client Name: ${body.name}\r\n
+                   Client Email: ${body.email}\r\n
+                   Client Phone: ${body.mobile_no}\r\n
+                   Service Category: ${body.service}\r\n
+                   Service sub category: ${body.sub_category_service}\r\n
+                  `;
 
   const data = {
-    to: "enquiries@proven.africa",
+    to: [
+      "enquiries@proven.africa",
+      "nelsonomoro@gmail.com",
+      "kevkiruthu@gmail.com",
+    ],
     from: "website@proven.africa",
     subject: "New Client Request",
-    text: message,
+    // text: message,
     html: message.replace(/\r\n/g, "<br/>"),
   };
 
-  mail.send(data);
+  mail.send(data).then(
+    () => {
+      res.send(200).json("Success");
+    },
+    (error) => {
+      console.error(error);
+
+      if (error.response) {
+        console.error(error.response.body);
+      }
+    }
+  );
 };
