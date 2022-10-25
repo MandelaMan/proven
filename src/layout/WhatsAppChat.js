371ea6d5f9b0ddb263386433f6df56c09d50dca9
MandelaMan <nelsonomoro@gmail.com>
2022-10-25 14:33:58 +0300

    total whatsapp removal

--- src/layout/WhatsAppChat.js
+++ src/layout/WhatsAppChat.js
@@ -3,19 +3,19 @@ import ReactWhatsapp from "react-whatsapp";
 const WhatsAppChat = () => {
   return (
     <div
-      style={{
-        position: "fixed",
-        width: "60px",
-        height: "60px",
-        bottom: "20px",
-        right: "20px",
-        backgroundColor: "#25d366",
-        color: "#FFF",
-        borderRadius: "50px",
-        textAlign: "center",
-        fontSize: "30px",
-        zIndex: 21474836478,
-      }}
+    // style={{
+    //   position: "fixed",
+    //   width: "60px",
+    //   height: "60px",
+    //   bottom: "20px",
+    //   right: "20px",
+    //   backgroundColor: "#25d366",
+    //   color: "#FFF",
+    //   borderRadius: "50px",
+    //   textAlign: "center",
+    //   fontSize: "30px",
+    //   zIndex: 21474836478,
+    // }}
     >
       {/* <ReactWhatsapp
         number="+25769937003"
